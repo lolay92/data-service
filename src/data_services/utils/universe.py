da184691368c2e62d76879b13fd093479e5faf6e
lolay92 <laleyecheryf@gmail.com>
2023-04-15 14:46:18 +0200

    data model for universe of tickers

--- src/data_services/utils/universe.py
+++ src/data_services/utils/universe.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from typing import List
 from dataclasses import dataclass, field
 from configparser import ConfigParser
@@ -6,68 +7,43 @@ UNIVERSE_FILEPATH = "src/data_services/utils/constants/universe.ini"
 
 
 @dataclass
-class Universe:
-    global_universe: ConfigParser = field(init=True, default_factory=ConfigParser)
-    us_eq_sector: str = field(init=False)
-    us_eq_index: str = field(init=False)
-    eq_dev_country: str = field(init=False)
-    eq_em_country: str = field(init=False)
-    us_fi_etfs: str = field(init=False)
-    commo_etfs: str = field(init=False)
+class UniverseQuery:
+    global_universe: ConfigParser = field(init=False, default_factory=ConfigParser)
+    # ----ETFS----
+    us_eq_sector: List = field(init=False)
+    us_eq_index: List = field(init=False)
+    eq_dev_country: List = field(init=False)
+    eq_em_country: List = field(init=False)
+    us_fi_etfs: List = field(init=False)
+    commo_etfs: List = field(init=False)
+    # ----FUTURES----
+    us_eq_idx_futs: List = field(init=False)
+    fx_futs: List = field(init=False)
+    fi_futs: List = field(init=False)
+    pm_futs: List = field(init=False)
+    energy_futs: List = field(init=False)
+    # ----FX PAIRS----
+    fx_major_pairs: List = field(init=False)
 
     def __post_init__(self):
         self.global_universe.read(UNIVERSE_FILEPATH)
-        # ------------------------ETFS-----------------------------------------------
+        # ----ETFS----
         self.us_eq_sector = self.global_universe["ETFS"]["US_EQ_SECTORS"].split(".")
         self.us_eq_index = self.global_universe["ETFS"]["US_EQ_INDEX"].split(".")
         self.eq_dev_country = self.global_universe["ETFS"]["EQ_DEV_COUNTRY"].split(".")
         self.eq_em_country = self.global_universe["ETFS"]["EQ_EM_COUNTRY"].split(".")
         self.us_fi_etfs = self.global_universe["ETFS"]["US_FI_ETFS"].split(".")
         self.commo_etfs = self.global_universe["ETFS"]["COMMO_ETFS"].split(".")
+        # ----FUTURES----
+        self.us_eq_idx_futs = self.global_universe["FUTS"]["US_EQ_IDX_FUTS"].split(".")
+        self.fx_futs = self.global_universe["FUTS"]["FX_FUTS"].split(".")
+        self.fi_futs = self.global_universe["FUTS"]["FI_FUTS"].split(".")
+        self.pm_futs = self.global_universe["FUTS"]["PM_FUTS"].split(".")
+        self.energy_futs = self.global_universe["FUTS"]["ENERGY_FUTS"].split(".")
+        # ----FX PAIRS----
+        self.fx_major_pairs = self.global_universe["FX"]["FX_MAJOR_PAIRS"].split(".")
+        # ----CRYPTO----
+        # To implement
 
     def print_global_universe(self):
-        for section in self.global_universe.sections():
-            print(f"\n[{section}]")
-            for key, value in self.global_universe.items(section):
-                print(f"{key} = {value}")
-
-    # def add_ticker_to_universe(self, universe: List):
-    #  pass
-
-
-# METHODS
-# print global universe
-# add element to universe
-# delete element in universe
-
-# Build: I could return one of defined universe or
-#   concat multiples universe
-#
-
-# Then I would be able to create with factory pattern something like this:
-#
-
-
-# def universe_components(self, universe) -> List:
-#     pass
-
-# def build(self) -> List:
-#     pass
-
-
-# @dataclass
-# class UniverseFactory(Universe):
-#     # create universe depending on the universe name
-#     pass
-
-
-if __name__ == "__main__":
-    universe = Universe()
-    print(universe.print_global_universe())
-    # pm_futs = universe.global_universe["FUTURES"]["PM_FUTURES"].split(".")
-    # print(pm_futs.split("."))
-    # print(universe.us_fi_etfs)
-    # print(universe.commo_etfs)
-    # print(universe.us_eq_index)
-
-    # print(universe.us_eq_sectors)
+        pprint(self.global_universe._sections)
