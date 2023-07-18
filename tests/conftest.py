487542bbd3d1eb184ca10e6dc3484baf683e2454
lolay92 <laleyecheryf@gmail.com>
2023-07-18 03:33:21 +0200

    ADD: adding a battery of tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,18 +2,44 @@ from datetime import date
 import pytest
 from src.data_services.loaders.eodhd import Eodhd
 from src.data_services.loaders.base import DataQuery
+from src.data_services.loader import Etf
+from enum import Enum
 
 
 @pytest.fixture
-def dummy_eodhd():
+def mock_eodhd_init_params():
     return Eodhd()
 
 
 @pytest.fixture
-def dummy_ohlcvquery():
+def mock_eodhd():
+    eodhd = Eodhd()
+    eodhd.params.update({"api_token": "demo"})
+    yield eodhd
+
+
+@pytest.fixture
+def mock_ohlcvquery():
     return DataQuery(
         exchange="US",
         start=date(2023, 1, 1),
-        end=date(2023, 5, 31),
+        end=date(2023, 1, 15),
         tickers=["MCD", "AAPL"],
     )
+
+
+@pytest.fixture
+def mock_universe():
+    class Universe_fixture(Enum):
+        DUMMY_ETF_UNIVERSE = (
+            "dummy_etf_universe",
+            "ETF",
+            "MCD.AAPL",
+        )
+
+    return Universe_fixture.DUMMY_ETF_UNIVERSE
+
+
+@pytest.fixture
+def mock_etf_loader():
+    return Etf()
--- tests/unit/loaders/test_eodhd.py
+++ tests/unit/loaders/test_eodhd.py
@@ -4,17 +4,17 @@ from unittest import mock
 from src.data_services.loaders.eodhd import InvalidEodKeyError
 
 
-def test_eodhd_init(dummy_eodhd):
+def test_eodhd_init(mock_eodhd_init_params):
     expected_params = {
-        "api_token": dummy_eodhd.api_key,
+        "api_token": mock_eodhd_init_params.api_key,
         "fmt": "json",
     }
     expected_root_url = "https://eodhistoricaldata.com/api"
     expected_API_name = "EODHISTORICALDATA"
 
-    assert dummy_eodhd.params == expected_params
-    assert dummy_eodhd.ROOT_URL == expected_root_url
-    assert dummy_eodhd.API.name == expected_API_name
+    assert mock_eodhd_init_params.params == expected_params
+    assert mock_eodhd_init_params.ROOT_URL == expected_root_url
+    assert mock_eodhd_init_params.API.name == expected_API_name
 
 
 @mock.patch.dict("src.data_services.loaders.base.os.environ", {}, clear=True)
@@ -35,29 +35,50 @@ def test_eodhd_invalidkeyerror():
 def test_search(
     mock_request_get,
     mock_response,
-    dummy_eodhd,
+    mock_eodhd,
     limit,
     search_query,
     json_response_value,
 ):
     mock_response.json.return_value = json_response_value
     mock_request_get.return_value = mock_response
-    assert dummy_eodhd.search(search_query, limit) == json_response_value
-    url = f"{dummy_eodhd.ROOT_URL}/search/{search_query}"
-    mock_request_get.assert_called_with(url=url, params=dummy_eodhd.params)
+    assert mock_eodhd.search(search_query, limit) == json_response_value
+    url = f"{mock_eodhd.ROOT_URL}/search/{search_query}"
+    mock_request_get.assert_called_with(url=url, params=mock_eodhd.params)
 
 
 @mock.patch("src.data_services.loaders.eodhd.requests.Response")
 @mock.patch("src.data_services.loaders.eodhd.requests.Session.get")
-def test_exchange_traded_tickers(mock_request_get, mock_response, dummy_eodhd):
+def test_exchange_traded_tickers(mock_request_get, mock_response, mock_eodhd):
     mock_response.json.return_value = [{"data": "example"}]
     mock_request_get.return_value = mock_response
 
     exchange_code = "US"
-    assert dummy_eodhd.exchange_traded_tickers(exchange_code) == [{"data": "example"}]
-    url = f"{dummy_eodhd.ROOT_URL}/exchange-symbol-list/{exchange_code}"
-    mock_request_get.assert_called_with(url=url, params=dummy_eodhd.params)
+    assert mock_eodhd.exchange_traded_tickers(exchange_code) == [{"data": "example"}]
+    url = f"{mock_eodhd.ROOT_URL}/exchange-symbol-list/{exchange_code}"
+    mock_request_get.assert_called_with(url=url, params=mock_eodhd.params)
 
 
-def test_ohlcv():
-    pass
+@pytest.mark.asyncio
+@mock.patch("src.data_services.loaders.eodhd.aiohttp.ClientSession.get")
+async def test_ohlcv(mock_get, mock_eodhd, mock_ohlcvquery):
+    # Mock the JSON responses
+    json_responses = [
+        [{"MCD": "response1"}, {"MCD": "response2"}],
+        [{"AAPL": "response1"}, {"AAPL": "response2"}],
+    ]
+
+    mock_get.return_value.__aenter__.return_value.json = mock.AsyncMock(side_effect=json_responses)
+
+    url1 = f"{mock_eodhd.ROOT_URL}/eod/{mock_ohlcvquery.tickers[0]}.{mock_ohlcvquery.exchange}"
+    url2 = f"{mock_eodhd.ROOT_URL}/eod/{mock_ohlcvquery.tickers[1]}.{mock_ohlcvquery.exchange}"
+
+    calls = [mock.call(url1, params=mock_eodhd.params), mock.call(url2, params=mock_eodhd.params)]
+
+    actual_result = await mock_eodhd.ohlcv(mock_ohlcvquery)
+
+    assert actual_result == json_responses
+
+    assert mock_get.call_count == 2
+    assert mock_get.return_value.__aenter__.return_value.json.await_count == 2
+    mock_get.assert_has_calls(calls, any_order=True)
